67a8b1ac4f7a248698b3a855e199853294d7ef8d
Vyom Jigar Gala <vyomjigargala@gmail.com>
2026-09-03 15:58:17 +0530

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class Settings:
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+    QDRANT_COLLECTION = "enterprise_rag"
+
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")
+    GROQ_MODEL = "llama-3.3-70b-versatile"
+
+
+
+settings = Settings()
